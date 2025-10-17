53871e589a1f16b158c8b67b6b3e66d7948a4fcb
Gregor Noczinski <gregor@noczinski.eu>
2025-10-17 11:15:43 +0200

    Defer ugly workaround of mise prior 2025.10.8.

--- hooks/post_install.lua
+++ hooks/post_install.lua
@@ -1,26 +1,39 @@
 function PLUGIN:PostInstall(ctx)
-    local raOk, archiver = pcall(require,"vfox.archiver")
-    if not raOk then
-        archiver = require("archiver")
+    local host = require("host")
+
+    local sdkInfo = ctx.sdkInfo[PLUGIN.name]
+
+    apply_ugly_workaround_if_required(sdkInfo)
+
+    local exe = host.path_join(sdkInfo.path, "bin", host.with_exec_ext("mongod"))
+    local success, err = pcall(host.exec, exe .. " -version")
+    if not success then
+        error(string.format("%s installation appears to be broken: got error while testing %s: %s", PLUGIN.name, exe, tostring(err)))
+    end
+end
+
+-- --------------------------------------------------------------------------------------------------
+-- Ugly workaround
+--
+-- ...because MISE prior 2025.10.8 cannot extract .tgz archives :-(
+function apply_ugly_workaround_if_required(sdkInfo)
+    -- If this not not MISE (usually vfox directly), immediately return...
+    if not host.is_mise() then
+        return
     end
 
     local host = require("host")
     local versions = require("versions")
+    local archiver = require("archiver")
 
-    local sdkInfo = ctx.sdkInfo[PLUGIN.name]
     local version = versions.get(sdkInfo.version)
     local path = sdkInfo.path
-
-    -- --------------------------------------------------------------------------------------------------
-    -- BEGIN: Ugly workaround
-    --
-    -- ...because not all archives are extracted automatically by MISE. Especially not: .tgz :-(
 
     local origArchiveFn = host.path_join(path, version.url:match("([^/\\]+)$"))
     local archiveFn = origArchiveFn:gsub("%.tgz$", ".tar.gz")
 
-    -- This will only work within MISE and if both file names are different.
-    if host.is_mise() and origArchiveFn ~= archiveFn then
+    -- This will only work if the source archive does exist and both filenames are different.
+    if host.can_read(origArchiveFn) and origArchiveFn ~= archiveFn then
         host.mv(origArchiveFn, archiveFn)
 
         local adOk = pcall(archiver.decompress, archiveFn, path)
@@ -40,14 +53,5 @@ done 2>/dev/null
 find '%s' -mindepth 1 -maxdepth 1 -type d -empty -delete 2>/dev/null
 ]]):format(path:gsub("'", "\\'")))
         end
-    end
-
-    -- END: Ugly workaround
-    -- --------------------------------------------------------------------------------------------------
-
-    local exe = host.path_join(path, "bin", host.with_exec_ext("mongod"))
-    local success, err = pcall(host.exec, exe .. " -version")
-    if not success then
-        error(string.format("%s installation appears to be broken: got error while testing %s: %s", PLUGIN.name, exe, tostring(err)))
     end
 end
